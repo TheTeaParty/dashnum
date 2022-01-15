62d419c0c917c06cb4aa39f44aeb8ab31790229e
Vlad Gorokhov <vlad@witchery.io>
2022-01-16 01:23:27 +0300

    testValidity Function Added

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
+import "regexp"
+
 // Task 1: Difficulty - 1/10; Estimate - 30 minutes
 
 func main() {
 
 }
 
+// testValidity` takes the string as an input, and returns boolean flag `true` if the given
+// string complies with the format, or `false` if the string does not comply
+func testValidity(str string) bool {
+	var re = regexp.MustCompile(`^([0-9]+-[a-zA-Z]+-?)+$`)
+	return re.MatchString(str)
+}
