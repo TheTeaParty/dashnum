e7bf0a63d44ba8629e0010371522cbc15b24ecb3
Vlad Gorokhov <vlad@witchery.io>
2022-01-16 01:28:59 +0300

    wholeStory Function Added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 // Task 1: Difficulty - 1/10; Estimate - 30 minutes
@@ -18,7 +19,7 @@ func testValidity(str string) bool {
 	return re.MatchString(str)
 }
 
-// averageNumber that takes the string, and returns the average number from all the numbers
+// averageNumber takes the string, and returns the average number from all the numbers
 func averageNumber(str string) float64 {
 	var re = regexp.MustCompile(`([0-9]+)-?`)
 	matches := re.FindAllStringSubmatch(str, -1)
@@ -30,4 +31,18 @@ func averageNumber(str string) float64 {
 	}
 
 	return float64(sum) / float64(len(matches))
+}
+
+// wholeStory takes the string, and returns a text that is composed of all the text words separated by
+// spaces, e.g. `story` called for the string `1-hello-2-world` should return text: `"hello world"
+func wholeStory(str string) string {
+	var re = regexp.MustCompile(`([a-zA-Z]+)-?`)
+	matches := re.FindAllStringSubmatch(str, -1)
+	var story []string
+
+	for _, match := range matches {
+		story = append(story, match[1])
+	}
+
+	return strings.Join(story, " ")
 }
